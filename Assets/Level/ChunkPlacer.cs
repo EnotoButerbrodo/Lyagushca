a989654164cbee8389a5e28180f2a2f31326cf14
Igor Vasilev <Protanker25@gmail.com>
2022-10-03 15:52:01 +1000

    Генератор уровня улучшен
    Создает со случайным офсетом между блоками в двух осях(требуется настройка велечин для баланса).  Префабы чанков удалил. Буду делать по нвоой системе

--- Assets/Level/ChunkPlacer.cs
+++ Assets/Level/ChunkPlacer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class ChunkPlacer : MonoBehaviour
@@ -7,32 +8,38 @@ public class ChunkPlacer : MonoBehaviour
     [SerializeField] private int seed;
     [SerializeField] private Transform _startPosition;
     [SerializeField] List<Chunk> _chunkPrefabs;
+    [SerializeField] private bool _randomSeed;
     
     private List<Chunk> _spawnedChunks = new List<Chunk>();
 
     private void Start()
     {
-        Random.InitState(seed);
-
+        
+        Random.InitState(_randomSeed ? System.DateTime.Now.Millisecond : seed);
+        
+        Generate();
+        
     }
 
-    [ContextMenu("Generate")]
     private void Generate()
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 50; i++)
         {
             SpawnChunk();
         }
     }
     private void SpawnChunk()
     {
-        int chunkNumber = Random.Range(0, _chunkPrefabs.Count - 1);
+        int chunkNumber = Random.Range(0, _chunkPrefabs.Count);
         Chunk spawnedChunk = Instantiate(_chunkPrefabs[chunkNumber]);
 
         Vector2 previousPoint = _spawnedChunks.Count == 0 ? _startPosition.position : _spawnedChunks.Last().EndPoint;
 
-        spawnedChunk.Link(previousPoint);
+        spawnedChunk.Link(previousPoint + GetRandomOffset());
         _spawnedChunks.Add(spawnedChunk);
     }
+
+    private Vector2 GetRandomOffset()
+        => Random.Range(1, 3f) * Vector2.right + Random.Range(-1.5f, 1.5f) * Vector2.up;
 
 }
