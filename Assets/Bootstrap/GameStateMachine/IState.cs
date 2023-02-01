64a1246d5ad2b8d2f9570886331ed2baafa579d4
Igor Vasilev <Protanker25@gmail.com>
2023-02-01 16:23:59 +1000

    Класс StateMachine(новый)

---
+++ Assets/Bootstrap/GameStateMachine/IState.cs
@@ -0,0 +1,17 @@
+﻿namespace Bootstrap.GameStateMachine
+{
+    public interface IState : IExitableState
+    {
+        void Enter();
+    }
+    
+    public interface IExitableState
+    {
+        void Exit();
+    }
+
+    public interface IPayloadedState<TPayload> : IExitableState
+    {
+        void Enter(TPayload payload);
+    }
+}
