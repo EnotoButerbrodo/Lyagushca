340f7fa71a69a4595c5742c69dca2a7ea89dc9e6
Igor Vasilev <Protanker25@gmail.com>
2023-02-05 17:09:32 +1000

    Изменен RegisterService

--- Assets/Codebase/Services/ResetService/IResetService.cs
+++ Assets/Codebase/Services/ResetService/IResetService.cs
@@ -1,38 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace Lyaguska.Services
+﻿namespace Lyaguska.Services
 {
-    public interface IResetService
+    public interface IResetService : IResetable
     {
-        void Reset();
         void Register(IResetable resetable);
-    }
-
-    public class ResetService : IResetService
-    {
-        private List<IResetable> _resetableObjects;
-
-        public ResetService()
-        {
-            _resetableObjects = new List<IResetable>(16);
-        }
-
-        public ResetService(List<IResetable> resetableObjects)
-        {
-            _resetableObjects = resetableObjects;
-        }
-
-        public void Reset()
-        {
-            foreach (IResetable resetableObject in _resetableObjects)
-            {
-                resetableObject.Reset();
-            }
-        }
-
-        public void Register(IResetable resetable)
-        {
-            _resetableObjects.Add(resetable);
-        }
     }
 }
---
+++ Assets/Codebase/Services/ResetService/ResetService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Lyaguska.Services
+{
+    public class ResetService : IResetService
+    {
+        private List<IResetable> _resetableObjects;
+
+        public ResetService()
+        {
+            _resetableObjects = new List<IResetable>(16);
+        }
+
+        public ResetService(List<IResetable> resetableObjects)
+        {
+            _resetableObjects = resetableObjects;
+        }
+
+        public void Reset()
+        {
+            foreach (IResetable resetableObject in _resetableObjects)
+            {
+                resetableObject.Reset();
+            }
+        }
+
+        public void Register(IResetable resetable)
+        {
+            _resetableObjects.Add(resetable);
+        }
+        
+    }
+}
