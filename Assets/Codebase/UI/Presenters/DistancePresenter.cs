b2f633d2440f87ceace70188f2718953bef23b40
Igor Vasilev <Protanker25@gmail.com>
2023-02-06 23:22:41 +1000

    Исправлен баг с TextMeshPro
    Не видел шейдеры. Решил, заменив строку с ошибкой #include "../../TextMesh Pro/Shaders/TMPro_Properties.cginc"

---
+++ Assets/Codebase/UI/Presenters/DistancePresenter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using Lyaguska.Services;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace Codebase.UI
+{
+    public class DistancePresenter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private string _format = "0.0";
+        [Inject] private IDistanceCountService _distanceCount;
+
+        private void OnEnable()
+        {
+            _distanceCount.DistanceChanged += OnDistanceChanged;
+        }
+
+        private void OnDisable()
+        {
+            _distanceCount.DistanceChanged -= OnDistanceChanged;
+        }
+
+        private void OnDistanceChanged(float distance)
+        {
+            _text.text = Mathf.FloorToInt(distance).ToString(_format, CultureInfo.InvariantCulture);
+        } 
+    }
+}
