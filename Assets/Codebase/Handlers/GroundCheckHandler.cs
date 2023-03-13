c6e73ce55abf8949ed98d049c6dd874c496c0b7f
Igor Vasilev <Protanker25@gmail.com>
2023-03-13 20:04:56 +1000

    Улучшено определение столкновения с землей
    Учитывается только прикосновения с поверхностями под определенным углом. Другими словами, столкновения со стеной больше не считается за приземление

--- Assets/Codebase/Handlers/GroundCheckHandler.cs
+++ Assets/Codebase/Handlers/GroundCheckHandler.cs
@@ -10,6 +10,10 @@ namespace Lyaguska.Handlers
         [SerializeField] private LayerMask _ground;
 
         private Collider2D[] _overlapResult = new Collider2D[1];
+        private ContactPoint2D[] _contacts = new ContactPoint2D[5];
+
+        private const float MinGroundAngle = 25;
+
         public bool IsGrounded()
         {
             return Physics2D.OverlapBoxNonAlloc(_collider.bounds.center, _collider.bounds.size, 0, _overlapResult, _ground) != 0;
@@ -17,7 +21,16 @@ namespace Lyaguska.Handlers
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            Grounded?.Invoke();
+            collision.GetContacts(_contacts);
+            foreach (ContactPoint2D contact in _contacts)
+            {
+                float angle = Vector2.Angle(transform.up, contact.normal);
+                if (angle <= MinGroundAngle)
+                {
+                    Grounded?.Invoke();
+                    return;
+                }
+            }
         }
 
 
