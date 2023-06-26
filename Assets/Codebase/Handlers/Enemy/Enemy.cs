9d8a3f705ea40c34c9d80745d2a6fb23432634d9
Igor Vasilev <Protanker25@gmail.com>
2023-06-26 17:22:49 +1000

    Spikes to enemy

--- Assets/Codebase/Handlers/Enemy/Enemy.cs
+++ Assets/Codebase/Handlers/Enemy/Enemy.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using Lyaguska.Actors;
+﻿using Lyaguska.Actors;
 using UnityEngine;
 
 namespace Lyaguska.Handlers
 {
-    public class Spikes : MonoBehaviour
+    public class Enemy : MonoBehaviour
     {
-        //Нанести урон лягушке при соприкосновении
         private void OnTriggerEnter2D(Collider2D other)
         {
-            Debug.Log("Spikes");
             var actor = other.GetComponent<Actor>();
             actor.Die();
         }
