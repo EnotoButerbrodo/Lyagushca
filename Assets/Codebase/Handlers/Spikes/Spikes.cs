37d37dce3b86484436c57819119d5a7925122bdf
Igor Vasilev <Protanker25@gmail.com>
2023-06-26 17:11:10 +1000

    Шипы смерти

---
+++ Assets/Codebase/Handlers/Spikes/Spikes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lyaguska.Actors;
+using UnityEngine;
+
+namespace Lyaguska.Handlers
+{
+    public class Spikes : MonoBehaviour
+    {
+        //Нанести урон лягушке при соприкосновении
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log("Spikes");
+            var actor = other.GetComponent<Actor>();
+            actor.Die();
+        }
+    }
+}
